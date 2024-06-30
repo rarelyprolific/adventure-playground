a6d0667a36b113c04c95532ad2d1b56e2ca6cc58
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2024-06-30 21:19:26 +0100

    Added a boring dotnet console app

---
+++ src/VeryBoringDotNetConsoleApp/VeryBoringDotNetConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace VeryBoringDotNetConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Says hello! Waits for a key press then exits. Does nowt else!");
+            Console.WriteLine("Press a key!");
+            Console.ReadKey();
+        }
+    }
+}
