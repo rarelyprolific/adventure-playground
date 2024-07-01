d5054c51cb806597ec4b42cd1801a52b5ca33c72
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2024-07-01 17:49:24 +0100

    Another code change

--- src/VeryBoringDotNetConsoleApp/VeryBoringDotNetConsoleApp/Program.cs
+++ src/VeryBoringDotNetConsoleApp/VeryBoringDotNetConsoleApp/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Says hello! Waits for a key press then exits. Does nowt else!!!!!!!!");
+            Console.WriteLine("Says hello! Waits for a key press then exits. Does nowt else!");
             Console.WriteLine("Press a key!");
             Console.ReadKey();
         }
